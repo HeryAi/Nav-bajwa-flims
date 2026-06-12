c3e202449b46a62228f48d137b74f8da14e02aca
Harry <harryinfo9@gmail.com>
2026-06-12 18:58:49 +0530

    update css

--- js/script.js
+++ js/script.js
@@ -135,3 +135,15 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+// Sticky Navbar 
+
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".main-navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+});
